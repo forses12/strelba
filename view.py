af3687217f9590729f1e9a140dc3af02a3f42fff
vassi <vassiagan@gmail.com>
2023-01-15 10:03:18 +0300

    ssss

--- view.py
+++ view.py
@@ -1,6 +1,9 @@
 import pygame
 
 screen=pygame.display.set_mode([1000,600],)
-
+a=pygame.image.load('images/space_gun1.png')
+a=pygame.transform.scale(a,[113,89],)
+a=pygame.transform.flip(a,True,False)
 def draw():
     pygame.display.flip()
+    screen.blit(a,[10,10])
