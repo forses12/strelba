8d6c3ecc9912f1220b03ab1c592e8beaae857a7e
vassi <asrtjlbv@ffgh.com>
2023-01-23 18:01:27 +0300

    BHBGHB

--- view.py
+++ view.py
@@ -1,9 +1,13 @@
-import pygame
+import pygame,model
 
 screen=pygame.display.set_mode([1000,600],)
+# screen=pygame.display.set_mode([0,0],pygame.FULLSCREEN)
 a=pygame.image.load('images/space_gun1.png')
 a=pygame.transform.scale(a,[113,89],)
 a=pygame.transform.flip(a,True,False)
+def rect(screen):
+    pygame.draw.rect(screen,[255,255,255],[25,25,950,550])
+rect(screen)
 def draw():
     pygame.display.flip()
     screen.blit(a,[10,10])
