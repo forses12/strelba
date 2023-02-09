c0144bde590712b2bc3553d096ec75bd5b738ded
vassi <asrtjlbv@ffgh.com>
2023-02-09 18:50:34 +0300

    BHBGHB

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ screen=pygame.display.get_surface()
 m=0
 r=[]
 j=''
-b =m + 3
-h =(screen.get_height() - 60) / b
 
 k=pygame.Rect([25, 25, screen.get_width() - 50, screen.get_height() - 50])
 def rect():
-    global r
+    global r,b,h
+    b =m + 3
+    h =(screen.get_height() - 60) / b
     for j in range(b):
         r.append(pygame.Rect([30, j * h + 30, screen.get_width() - 60, h]))
 rect()
